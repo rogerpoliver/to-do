acecac28c9d7b39aba23f9ab3e8e6d00ccb161bf
Roger Oliveira <rogerpolvr@gmail.com>
2021-04-01 21:59:13 -0300

    feat(03): Enabling the delete task button

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,7 +15,6 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState("");
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
     if (!newTaskTitle) return;
 
     const newTask = {
@@ -29,7 +28,6 @@ export function TaskList() {
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
     const taskList = tasks.map((task) =>
       task.id === id
         ? {
@@ -43,7 +41,8 @@ export function TaskList() {
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    const taskList = tasks.filter((task) => task.id !== id);
+    setTasks(taskList);
   }
 
   return (
